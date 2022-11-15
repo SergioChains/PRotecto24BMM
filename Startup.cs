cdc23c49e18092c1edc44e926399eadb91679a9e
Upqroo <Upqroo@DESKTOP-VB74L91>
2022-11-15 08:06:21 -0500

    Se creo jquary

--- Startup.cs
+++ Startup.cs
@@ -55,7 +55,7 @@ namespace WebApplication1
             {
                 endpoints.MapControllerRoute(
                     name: "default",
-                    pattern: "{controller=Home}/{action=Index}/{id?}");
+                    pattern: "{controller=Login}/{action=Login}/{id?}");
             });
         }
     }
